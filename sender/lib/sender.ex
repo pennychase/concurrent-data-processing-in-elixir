aff4d74f0bbe3ce9ec7474f19c929dc1785480bb
Penny Chase <mpchase@mac.com>
2026-09-23 22:30:36 -0400

    Used supervisor with Task

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -3,6 +3,9 @@ defmodule Sender do
   Documentation for `Sender`.
   """
 
+  # To test supervisor
+  def send_email("konnichiwa@world.com" = email), do: raise "Oops, couldn't send enail to #{email}!"
+
   def send_email(email) do
     Process.sleep(3000)
     IO.puts("Email to #{email} sent")
@@ -10,8 +13,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
     |> Enum.to_list()
   end
 
